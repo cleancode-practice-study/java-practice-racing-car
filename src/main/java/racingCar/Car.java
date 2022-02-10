9f02e21728480d87d28e341c69fe01f18888484b
Egenieee <hhgg0925@naver.com>
2022-02-10 20:25:55 +0900

    feat: 경주 횟수 검증 로직 추가 fix: 자동차 이름 유효 검증 로직 수정 fix: Car 객체 출력 로직 수정

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -1,46 +1,48 @@
 package racingCar;
 
 public class Car {
-	public static int CAN_DRIVE = 4;
+    private static int CAR_DRIVE = 4;
 
-	private final String name;
-	private int position = 0;
+    private final String name;
+    private int position = 0;
 
-	public Car(String name) {
-		this.name = name;
-	}
+    public Car(String name) {
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public int getPosition() {
-		return position;
-	}
+    public int getPosition() {
+        return position;
+    }
 
-	public void move(int randomNum) {
-		if (randomNum >= CAN_DRIVE) {
-			this.position++;
-		}
-	}
+    public void move(int randomNum) {
+        if (randomNum >= CAR_DRIVE) {
+            this.position++;
+        }
+    }
 
-	// 현재 포지션 만큼 결과 출력
-	public String getResult() {
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(name + " : ");
-		if (position > 0) {
-			stringBuilder.append(printProgress(position));
-		}
+    // 현재 포지션 만큼 결과 출력
+    public String getResult() {
+        StringBuilder stringBuilder = new StringBuilder();
 
-		return stringBuilder.toString();
-	}
+        stringBuilder.append(name + " : ");
 
-	public String printProgress(int position) {
-		StringBuilder stringBuilder = new StringBuilder();
-		for(int i = 0 ; i < position ; i++) {
-			stringBuilder.append("-");
-		}
-		return stringBuilder.toString();
-	}
+        stringBuilder.append(printProgress(position));
+
+        return stringBuilder.toString();
+    }
+
+    private String printProgress(int position) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < position; i++) {
+            stringBuilder.append("-");
+        }
+
+        return stringBuilder.toString();
+    }
 
 }
